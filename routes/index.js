9c223ae039446dff0254a736c3a97f1f20101e10
Juan J. Alvarez <jalvarez5241997@gmail.com>
2016-10-16 20:35:11 -0700

    Changed routes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/t', function(req, res, next){
+router.get('/commenttest', function(req, res, next){
 	db.Comment.find({})
 	.populate('author')
 	.exec(function(err, comments){
@@ -15,6 +15,44 @@ router.get('/t', function(req, res, next){
 		};
 		res.render('commenttest', json);
 	});
+});
+
+router.get('/register', function(req, res, next){
+	res.render('register');
+});
+
+router.post('/register', function(req, res, next){
+	var un = req.body.username;
+	var existingUser = db.User.findOne({'username':un});
+	if(existingUser.length > 0){
+		res.render('redirect', {
+			title: 'Could not register!',
+			content: 'A user with the same username already exists'
+		});
+		return;
+	}
+	var newUser = new db.User({
+		username: un,
+		name: req.body.name,
+		password: req.body.password
+	});
+	newUser.save(function(err, usr){
+		if(err)
+			console.log(err);
+	});
+	res.render('redirect', {
+		title: 'Successfully registered'
+	});
+});
+
+router.post('/login', function(req, res, next){
+	var un = req.body.username;
+	var pw = req.body.password;
+	var user = db.User.findOne({'username':un});
+	if(user){
+		if(pw === user.password)
+			console.log('valid login');
+	}
 });
 
 module.exports = router;
