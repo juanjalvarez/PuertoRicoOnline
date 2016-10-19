b239fa9d555ab1831ef37702da76da91d4453294
Juan J. Alvarez <jalvarez5241997@gmail.com>
2016-10-19 10:34:10 -0400

    Integrated sessions with sockets

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -23,7 +27,7 @@ var session = express_session({
 module.exports.session = session;
 
 var hbs = exphbs.create({
-  defaultLayout: 'main'
+ defaultLayout: 'main'
 });
 
 var app = express();
@@ -56,9 +60,9 @@ app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
 });
 
 // error handlers
@@ -66,24 +70,115 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+ app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+     message: err.message,
+     error: err
+   });
+ });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+ res.status(err.status || 500);
+ res.render('error', {
+   message: err.message,
+   error: {}
+ });
+});
+var debug = require('debug')('PuertoRicoOnline:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = 8081;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port, function(){
+	console.log('Server started on port ' + port);
+});
+server.on('error', onError);
+server.on('listening', onListening);
+
+var io = require('socket.io')(server);
+
+io.use(function(socket, next){
+	session(socket.request, socket.request.res, next);
 });
 
+module.exports.io = io;
+require('./controllers/socket.js');
 
-module.exports = app;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
--- controllers/socket.js
+++ controllers/socket.js
@@ -1,16 +1,15 @@
-var www = require('../bin/www');
-var io = www.io;
-var session = www.app.session;
-//var expresssession = require('express-socket.io-session');
+var app = require('../app');
+var io = app.io;
+var session = app.session;
 
 console.log('Starting socket server');
 
-//io.use(expresssession(session));
-
 io.on('connection', function(socket){
 	console.log('new connection');
+
 	socket.on('message', function(msg){
 		console.log(msg);
+		socket.emit('r', socket.request.session.auth.username);
 	});
 });
 
